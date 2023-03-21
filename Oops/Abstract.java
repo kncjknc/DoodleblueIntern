5472311c95d800e53e09a9e11a3e8c8340d3b821
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-03-21 13:13:11 +0530

    Create Abstract.java

---
+++ Oops/Abstract.java
@@ -0,0 +1,28 @@
+package abstrac;
+
+public class Abstract_Describe extends Abstract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	void input() {
+		
+				
+	}
+	
+   void mul() {
+		
+	}
+
+
+   void add() {
+	
+   }
+
+   void sub() {
+	
+   }
+
+}
