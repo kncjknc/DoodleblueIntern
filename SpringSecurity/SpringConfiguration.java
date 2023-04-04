d79892176bcf300a5040c727d76ee4f801ca3574
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-04-04 20:43:33 +0530

    Create SpringConfiguration.java

---
+++ SpringSecurity/SpringConfiguration.java
@@ -0,0 +1,58 @@
+package CrudQueryWithSecurity.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfigure {
+	
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public UserDetailsService userDeailService(PasswordEncoder encoder) {
+		
+		UserDetails admin = User.withUsername("varathan")
+				.password(encoder.encode("PWD1")).
+				roles("ADMIN").build();
+		
+		UserDetails hr = User.withUsername("arjun")
+				.password(encoder.encode("PWD2")).
+				roles("HR").build();
+		return new InMemoryUserDetailsManager(admin,hr);
+		
+		//return new UserInfoService();
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		return http.csrf().disable()
+		.authorizeHttpRequests().requestMatchers("/student/{id}").permitAll()
+		.and().authorizeHttpRequests().requestMatchers("/getStudents")
+		.authenticated().and().formLogin().and().build();
+		
+	}
+	
+
+
+}
