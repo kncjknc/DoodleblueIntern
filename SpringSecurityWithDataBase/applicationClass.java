3660d62cc25a949f5e98bddddb7f8d6f2cc13dd1
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-04-05 11:20:58 +0530

    Create applicationClass.java

---
+++ SpringSecurityWithDataBase/applicationClass.java
@@ -0,0 +1,14 @@
+package CrudQueryWithSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CrudOperQueryWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperQueryWithSpringSecurityApplication.class, args);
+	}
+
+}
