8dd70d08303c50eebceab1665cd4af617801bb8f
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-04-08 08:27:51 +0530

    Create UserRepository.java

---
+++ SpringSecurityInJWT/Repository/UserRepository.java
@@ -0,0 +1,15 @@
+package SpringSecurityInJwt.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import SpringSecurityInJwt.entity.UserInfo;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+
+	Optional<UserInfo> findByName(String username);
+
+}
