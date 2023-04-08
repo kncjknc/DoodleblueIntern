16c94c3aa5329c6e40d0482908ada0407410eec9
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-04-08 08:19:01 +0530

    Create ApplicationJWT.java

---
+++ SpringSecurityInJWT/ApplicationJWT.java
@@ -0,0 +1,13 @@
+package SpringSecurityInJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInJwtApplication.class, args);
+	}
+
+}
