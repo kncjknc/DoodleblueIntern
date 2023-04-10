17359c02d32bce2745f8dc578237b4683bbcc678
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-04-10 10:11:50 +0530

    Create AuthTokenClass.java

---
+++ EmployeesCRUDproject/Model/AuthTokenClass.java
@@ -0,0 +1,37 @@
+package EmployeeManageMentSystem.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthToken {
+
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public AuthToken(String username, String password) {
+
+		this.username = username;
+		this.password = password;
+	}
+
+	public AuthToken() {
+
+	}
+
+}
