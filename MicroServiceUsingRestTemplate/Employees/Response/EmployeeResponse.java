0c2642c42fb669530146fee796ed7290b3ff8ca6
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-04-17 16:16:22 +0530

    Create EmployeeResponse.java

---
+++ MicroServiceUsingRestTemplate/Employees/Response/EmployeeResponse.java
@@ -0,0 +1,64 @@
+package Employee.responce;
+
+public class EmployeeResponce {
+
+	private int id;
+	private String name;
+	private int salary;
+	private int age;
+	private EmployeeAddressResponse employeeAddressResponse;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public EmployeeAddressResponse getEmployeeAddressResponse() {
+		return employeeAddressResponse;
+	}
+
+	public void setEmployeeAddressResponse(EmployeeAddressResponse employeeAddressResponse) {
+		this.employeeAddressResponse = employeeAddressResponse;
+	}
+
+	public EmployeeResponce(int id, String name, int salary, int age, EmployeeAddressResponse employeeAddressResponse) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.age = age;
+		this.employeeAddressResponse = employeeAddressResponse;
+	}
+
+	public EmployeeResponce() {
+		super();
+	}
+
+}
