2ecdace692555431c5da5dd0914beda7ea6a6318
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-04-17 16:12:19 +0530

    Create EmployeeConfig.java

---
+++ MicroServiceUsingRestTemplate/Employees/EmployeeConfig.java
@@ -0,0 +1,20 @@
+package Employee.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EmployeeConfig {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+//	
+//	@Bean
+//	public RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
+
+}
