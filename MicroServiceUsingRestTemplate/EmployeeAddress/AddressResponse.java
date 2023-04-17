d018bd1e2feeb4f533ce26d7e4f64204aba8ee0a
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-04-17 16:35:39 +0530

    Create AddressResponse.java

---
+++ MicroServiceUsingRestTemplate/EmployeeAddress/AddressResponse.java
@@ -0,0 +1,46 @@
+package Employee_address.response;
+
+public class EmployeeAddressResponse {
+
+	private int addressID;
+
+	private String street;
+
+	private String city;
+
+	public int getAddressID() {
+		return addressID;
+	}
+
+	public void setAddressID(int addressID) {
+		this.addressID = addressID;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public EmployeeAddressResponse(int addressID, String street, String city) {
+
+		this.addressID = addressID;
+		this.street = street;
+		this.city = city;
+	}
+
+	public EmployeeAddressResponse() {
+
+	}
+
+}
