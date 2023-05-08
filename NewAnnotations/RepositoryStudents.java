bee274de124024f6debf0cfbe49c9bab119043d3
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-05-08 18:33:52 +0530

    Create RepositoryStudents.java

---
+++ NewAnnotations/RepositoryStudents.java
@@ -0,0 +1,10 @@
+package com.example.demo.repositary;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Students;
+
+public interface StudentsRepositary extends JpaRepository<Students, Integer> {
+
+}
