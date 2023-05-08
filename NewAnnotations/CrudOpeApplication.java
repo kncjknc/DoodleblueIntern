011ae180062b0612291a670cbfb99d22c252d470
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-05-08 18:31:56 +0530

    Create CrudOpeApplication.java

---
+++ NewAnnotations/CrudOpeApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@SpringBootApplication
+@OpenAPIDefinition(info=@Info(title="Students Controller",version="1.0",description="Students Management "))
+public class CrudOpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOpeApplication.class, args);
+	}
+
+}
