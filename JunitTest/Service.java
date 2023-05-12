e9c2bf49016a3c9832623ecdc67ed23fbff08e52
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-05-12 18:39:17 +0530

    Create Service.java

---
+++ JunitTest/Service.java
@@ -0,0 +1,71 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.Students;
+import com.example.demo.repositary.StudentsRepositary;
+
+@Service
+public class StudentsService {
+
+	@Autowired
+	private StudentsRepositary repository;
+
+	public Students saveDetail(Students student) {
+		return repository.save(student);
+	}
+
+	public List<Students> saveDetails(List<Students> students) {
+		return repository.saveAll(students);
+	}
+
+	public List<Students> getStudents() {
+		return repository.findAll();
+	}
+
+	public Students getStudentById(int id) {
+		return repository.findById(id).orElse(null);
+	}
+
+	public Students getStudentByName(String name) {
+		return repository.findByName(name);
+	}
+
+	public String deleteStudent(int id) {
+		repository.deleteById(id);
+		return "Student Details Dissmis !";
+	}
+
+	public List<Students> getOrderByNames() {
+		return repository.getOrderByNames();
+	}
+
+	public List<Students> getDetailUsingNameorId(String name, int id) {
+		return repository.getDetailUsingNameorId(name, id);
+	}
+
+	public List<Students> getAllDeatilUsingNative() {
+		return repository.getAllDeatilUsingNative();
+	}
+
+	public List<Students> getExactDeatail(String k) {
+		return repository.getExactDeatail(k);
+	}
+
+	// Named Query Define
+
+	public Students findByStd(String s) {
+		return repository.findByStd(s);
+	}
+
+	public Students updateStudents(Students student) {
+		Students exisitingStudent = repository.findById(student.getId()).orElse(null);
+		exisitingStudent.setName(student.getName());
+		exisitingStudent.setCity(student.getCity());
+		exisitingStudent.setStd(student.getStd());
+		return repository.save(exisitingStudent);
+	}
+
+}  
