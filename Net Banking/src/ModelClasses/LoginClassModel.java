26dccc63087e8b3f563438e10768f2041693187d
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-05-18 11:15:09 +0530

    Create LoginClassModel.java

---
+++ Net Banking/src/ModelClasses/LoginClassModel.java
@@ -0,0 +1,69 @@
+package InternetBank.model;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import InternetBank.entity.UsersInfo;
+
+public class UserInfoDetails implements UserDetails {
+
+	private String clientname;
+	private String password;
+	private List<GrantedAuthority> authorities;
+
+	public UserInfoDetails(UsersInfo usersInfo) {
+		clientname = usersInfo.getClientname();
+		password = usersInfo.getPassword();
+		authorities = Arrays.stream(usersInfo.getRoles().split(",")).map(SimpleGrantedAuthority::new)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return clientname;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
