b3ad974706464af4160ece82212c965c3b2aae1f
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-05-18 11:16:47 +0530

    Create TransactionRepo.java

---
+++ Net Banking/src/Repository/TransactionRepo.java
@@ -0,0 +1,15 @@
+package InternetBank.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import InternetBank.entity.TransactionDetails;
+
+public interface TransactionRepository extends JpaRepository<TransactionDetails, Integer> {
+
+	@Query(nativeQuery = true, value = "SELECT * FROM internetbanking.transaction_details where account_no =:accNo")
+	List<TransactionDetails> getTransactionDetail(int accNo);
+
+}
