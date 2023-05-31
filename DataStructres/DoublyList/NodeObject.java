90624be58007edcfe869cf80f76e88ec64e3ef20
varathan1042002 <128460563+varathan1042002@users.noreply.github.com>
2023-05-31 21:24:36 +0530

    Create NodeObject.java

---
+++ DataStructres/DoublyList/NodeObject.java
@@ -0,0 +1,16 @@
+package Package2;
+
+public class Nodes {
+	
+	int data;
+	Nodes next;
+	Nodes prev;
+	
+	public Nodes(int data) {
+		this.data=data;
+		this.next=null;
+		this.prev=null;
+	}
+	
+
+}
